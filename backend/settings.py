8caee07ee759e77d37c729b283f5f1da2454d663
Znick85 <67632302+Znick85@users.noreply.github.com>
2025-06-14 10:56:06 +0200

    Add files via upload

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-haaqk1_%qij100jb9g2ly779z1s7*j+00#d$x@bvf1j6+_9saq
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['192.168.1.35', 'localhost', '130.25.12.189','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.35', 'localhost', '47.53.162.51','127.0.0.1']
 
 
 
